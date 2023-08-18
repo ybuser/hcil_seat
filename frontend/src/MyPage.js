4a633938bcf8ae5d980f5a4e61d1e15051f7aaa3
Yunbo <ybuser@naver.com>
2023-08-18 17:59:01 +0900

    add: frontend more work, mini squares, viewer/editor mode, intern tables

--- frontend/src/MyPage.js
+++ frontend/src/MyPage.js
@@ -33,14 +33,36 @@ const SquareButton2 = styled.button`
   width: 60px;
   height: 60px; /* Makes the button square */
   margin: 1%;
-  background-color: #2f5597;
+  background-color: ${props => (props.children ? '#385723' : '#2f5597')};
   color: white;
-  cursor: pointer;
+  cursor: ${props => (props.isEditorMode ? 'pointer' : 'default')};
   border: none;
   border-radius: 10%;  // Rounded corners for the button
   &:hover {
-    background-color: #002060;
+    background-color: ${props => (props.isEditorMode ? "#002060" : (props.children ? '#385723' : '#2f5597'))}; 
   }
+`;
+
+const TableWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MiniSquareRow = styled.div`
+  display: flex;
+  justify-content: center; // Horizontally align the mini squares
+  margin: 5px 0;
+  position: relative;
+`;
+
+const MiniSquare = styled.div`
+  width: 18px;
+  height: 18px; 
+  margin: 1%;
+  background-color: ${props => props.color ? '#FFA500' : '#2f5597'};
+  border-radius: 10%;
 `;
 
 const MainContent = styled.div`
@@ -67,12 +89,35 @@ const RightBarSection = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
+  height: 100%; // take the full height of TopBar
+  
   button {
-    padding: 5px 10px;
+    padding: 10px 20px;
     margin: 0 5px;
     cursor: pointer;
+    border-radius: 20px;
+    color: white;
+    font-size: 16px;
+    background-color: #767171;
+    border: none;
+    transition: background-color 0.3s; // Smooth transition for background color change
+    
+    &:hover {
+      opacity: 0.9; // Slight fade on hover
+    }
   }
 `;
+
+const selectedButtonStyle = {
+  backgroundColor: '#548235',
+  border: '2px solid #c2ddb3'
+};
+
+const unselectedButtonStyle = {
+  backgroundColor: '#767171',
+  border: 'none'
+};
 
 const Modal = styled.div`
   position: fixed;
@@ -95,35 +140,37 @@ const VerticalTable = styled.div`
   position: absolute;
   width: 60px;
   height: 90px;
-  background-color: #afabab;
+  background-color: ${props => (props.isSelected ? "#FFA500" : "#afabab")};
+  cursor: ${props => (props.isEditorMode ? "pointer" : "default")};
+  &:hover {
+    background-color: ${props => (props.isEditorMode ? "#e89c5c" : (props.isSelected ? "#FFA500" : "#afabab"))}; 
+  }
   &:nth-child(11) { top: 20px; left: 30px; }
   &:nth-child(12) { top: 120px; left: 30px; }
   &:nth-child(13) { top: 260px; left: 30px; }
   &:nth-child(14) { top: 360px; left: 30px; }
 `;
 
-
 const HorizontalTable = styled.div`
   position: absolute;
   width: 120px;
   height: 80px;
-  background-color: #f8cbad;
-  //on hover #ed7241
+  background-color: ${props => (props.isSelected ? "#ed7241" : "#f8cbad")};
   
   cursor: pointer;
   &:hover {
     background-color: #ed7241;
   }
-  &:nth-child(1) { top: 130px; left: 170px; }
-  &:nth-child(2) { top: 130px; left: 305px; }
-  &:nth-child(3) { top: 130px; left: 440px; }
-  &:nth-child(4) { top: 130px; left: 575px; }
-  &:nth-child(5) { top: 130px; left: 710px; }
-  &:nth-child(6) { top: 225px; left: 170px; }
-  &:nth-child(7) { top: 225px; left: 305px; }
-  &:nth-child(8) { top: 225px; left: 440px; }
-  &:nth-child(9) { top: 225px; left: 575px; }
-  &:nth-child(10) { top: 225px; left: 710px; }  // Example for the last table in 2nd row
+  /* &:nth-child(1) { top: 0px; left: 0px; }
+  &:nth-child(2) { top: 0px; left: 135px; }
+  &:nth-child(3) { top: 0px; left: 270px; }
+  &:nth-child(4) { top: 0px; left: 405px; }
+  &:nth-child(5) { top: 0px; left: 540px; }
+  &:nth-child(6) { top: 95px; left: 0px; }
+  &:nth-child(7) { top: 95px; left: 135px; }
+  &:nth-child(8) { top: 95px; left: 270px; }
+  &:nth-child(9) { top: 95px; left: 405px; }
+  &:nth-child(10) { top: 95px; left: 540px; } */
 `;
 
 const Page = () => {
@@ -133,6 +180,10 @@ const Page = () => {
   const [showTables, setShowTables] = useState(false);
   const [tableSelected, setTableSelected] = useState(false);
   const [selectedTable, setSelectedTable] = useState(null);
+  const [selectedVerticalTables, setSelectedVerticalTables] = useState([]);
+  // Default to empty names for each square for each of the tables
+  const defaultNames = Array.from({ length: 10 }, () => Array(7).fill(''));
+  const [buttonNames, setButtonNames] = useState(defaultNames);
 
   const handleModeChange = (newMode) => {
     if (newMode === 'editor') {
@@ -150,46 +201,123 @@ const Page = () => {
     setShowModal(false);
   };
 
+  const handleVerticalTableClick = (index) => {
+    if (selectedVerticalTables.includes(index)) {
+      // If the index is already in the array, remove it
+      setSelectedVerticalTables(prev => prev.filter(i => i !== index));
+    } else {
+      // If the index is not in the array, add it
+      setSelectedVerticalTables(prev => [...prev, index]);
+    }
+  };
+
   return (
     <PageWrapper>
       <Sidebar>
-        <SquareButton isSelected={selectedPage === 'page1'} onClick={() => {setSelectedPage('page1'); setShowTables(true);}}>Page 1</SquareButton>
-        <SquareButton isSelected={selectedPage === 'page2'} onClick={() => {setSelectedPage('page2'); setShowTables(false);}}>Page 2</SquareButton>
+        <SquareButton isSelected={selectedPage === 'page1'} onClick={() => {setSelectedPage('page1'); setShowTables(true);}}>삼성</SquareButton>
+        <SquareButton isSelected={selectedPage === 'page2'} onClick={() => {setSelectedPage('page2'); setShowTables(false);}}>제2공</SquareButton>
       </Sidebar>
       <MainContent>
         <TopBar>
-            <ButtonGroup>
-                <button onClick={() => handleModeChange('viewer')}>Viewer</button>
-                <button onClick={() => handleModeChange('editor')}>Editor</button>
+            <ButtonGroup style={{width:'25%'}}>
+              <button 
+                style={mode === 'viewer' ? selectedButtonStyle : unselectedButtonStyle}
+                onClick={() => handleModeChange('viewer')}
+              >
+                Viewer
+              </button>
+              <button 
+                style={mode === 'editor' ? selectedButtonStyle : unselectedButtonStyle}
+                onClick={() => handleModeChange('editor')}
+              >
+                Editor
+              </button>
             </ButtonGroup>
             {tableSelected && ( // Display only if a table is selected
             <RightBarSection>
-                {[...Array(7)].map((_, i) => (
-                <SquareButton2 key={i}></SquareButton2>
-                ))}
+              {selectedTable !== null && buttonNames[selectedTable].map((name, i) => (
+                <SquareButton2 
+                  key={i}
+                  isEditorMode={mode === 'editor'}
+                  onClick={() => {
+                    if (mode !== 'editor') return; // Do nothing if not in editor mode
+                
+                    if (name) {
+                      const confirmErase = window.confirm('Do you want to erase the name?');
+                      if (confirmErase) {
+                        const updatedNames = [...buttonNames];
+                        updatedNames[selectedTable][i] = ''; // Clear the name for the selected table's square
+                        setButtonNames(updatedNames);
+                      }
+                    } else {
+                      const newName = prompt('Enter name:');
+                      if (newName) {
+                        const updatedNames = [...buttonNames];
+                        updatedNames[selectedTable][i] = newName;
+                        setButtonNames(updatedNames);
+                      }
+                    }
+                  }}
+                >
+                  {name}
+                </SquareButton2>
+              ))}
             </RightBarSection>
             )}
         </TopBar>
         {showTables ? (
             <TableGroup>
                 {[...Array(10)].map((_, i) => (
-                <HorizontalTable key={i} onClick={() => {
-                    if (selectedTable === i) {
-                        setSelectedTable(null);
-                        setTableSelected(false);
-                    } else {
-                        alert(`Horizontal table ${i + 1} clicked`)
-                        setSelectedTable(i);
-                        setTableSelected(true);
-                    }
-                }} />
+                  <TableWrapper 
+                    key={i} 
+                    style={{ 
+                      top: i < 5 ? '130px' : '225px', 
+                      left: `${170 + 135 * (i % 5)}px` 
+                    }}
+                  >
+                    {/* Render mini squares above the table for the first row */}
+                    {i < 5 && (
+                      <MiniSquareRow style={{ top: '-35px' }}>
+                        {buttonNames[i] && buttonNames[i].map((name, idx) => (
+                          <MiniSquare color={name} key={idx} />
+                        ))}
+                      </MiniSquareRow>
+                    )}
+                    <HorizontalTable 
+                      isSelected={i === selectedTable} 
+                      onClick={() => {
+                        if (selectedTable === i) {
+                          setSelectedTable(null);
+                          setTableSelected(false);
+                        } else {
+                          setSelectedTable(i);
+                          setTableSelected(true);
+                        }
+                      }} 
+                    />
+                    {/* Render mini squares below the table for the second row */}
+                    {i >= 5 && (
+                      <MiniSquareRow style={{ top: '82px' }}>
+                        {buttonNames[i] && buttonNames[i].map((name, idx) => (
+                          <MiniSquare color={name} key={idx} />
+                        ))}
+                      </MiniSquareRow>
+                    )}
+                  </TableWrapper>
                 ))}
                 {[...Array(4)].map((_, i) => (
-                <VerticalTable key={i} />
+                  <VerticalTable 
+                    key={i} 
+                    isSelected={selectedVerticalTables.includes(i)} // Check if current table is in the selected array
+                    isEditorMode={mode === 'editor'}
+                    onClick={() => {
+                      if (mode === 'editor') handleVerticalTableClick(i);
+                    }} 
+                  />
                 ))}
             </TableGroup>
                 ) : (
-            <div>Other content for Page 2</div>
+            <div>제2공학관 연구실은 아직 페이지 공사 중입니다.</div>
         )}
       </MainContent>
       {showModal && (
